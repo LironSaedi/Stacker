52e7b06a9d94f380ec4d7e25205c109fcd8fc77c
LironSaedi <lionsaedi@gmail.com>
2018-11-16 17:32:55 -0800

    Inital Commit

---
+++ Stacker/Stacker/FixedObject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacker
+{
+
+    class FixedObject
+    {
+        Vector2 position;
+        Texture2D texture;
+        Vector2 scale;
+        Color Tint;
+        Collider Hitbox;
+        SpriteBatch batch;
+
+    }
+}
---
+++ Stacker/Stacker/MovingObject.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacker
+{
+
+    //FixedObject class: position, texture, scale, color, hitbox, Draw()
+    //MovingObject : FixedObject + Speed, Update()
+    class MovingObject
+    {
+        Vector2 Position;
+        Color color;
+        Texture2D texture;
+        Vector2 Scale;
+        Vector2 Speed;
+        int Score;
+        Collider Hitbox
+
+
+        public MovingObject(Texture2D image, Vector2 position, Color color, Vector2 speed)
+        {
+            this.Speed = speed;
+            Score = 0;
+
+            Hitbox = new Collider(texture, Position + new Vector2(texture.Width * Scale.X / 2, texture.Height * Scale.Y / 2));
+        }
+
+    }
+}
+
